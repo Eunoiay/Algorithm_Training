0df1622db9b86a9dae75dd80b2ce407de86e2f50
Eunoiay <648344219@qq.com>
2025-09-14 16:03:48 +0800

    20250914 Eunoiay's submission for CF164A

---
+++ daily_problems/2025/09/0903/personal_submission/cf164a_eunoiay.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using i64 = long long;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> f(n);
+    for (int i = 0; i < n; i++) {
+        cin >> f[i];
+    }
+    
+    vector<vector<int>> adj(n), radj(n);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        --u, --v;
+        adj[u].push_back(v);
+        radj[v].push_back(u);
+    }
+    
+    queue<int> q;
+    vector<int> vis(n);
+    for (int i = 0; i < n; i++) {
+        if (f[i] == 1) {
+            vis[i] |= 1;
+            q.push(i);
+        }
+    }
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v : adj[u]) {
+            if (f[v] != 1 && (vis[v] & 1) == 0) {
+                q.push(v);
+            }
+            vis[v] |= 1;
+        }
+    }
+    
+    q = queue<int>();
+    for (int i = 0; i < n; i++) {
+        if (f[i] == 2) {
+            vis[i] |= 2;
+            q.push(i);
+        }
+    }
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (int v : radj[u]) {
+            if (f[v] != 1 && (vis[v] & 2) == 0) {
+                q.push(v);
+            }
+            vis[v] |= 2;
+        }
+    }
+    
+    for (int i = 0; i < n; i++) {
+        cout << (vis[i] == 3 ? 1 : 0) << "\n";
+    }
+    
+    return 0;
+}
